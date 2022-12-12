6e6082b6113f007a6dc76bdd6dfb83919baa5f0f
spxrtzy <93079460+spxrtzy@users.noreply.github.com>
2022-12-12 18:48:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+print("\nWelcome, please fill out the info")
+
+HEIGHT = input("Enter Height: ")
+WIDTH = input("Enter Width: ")
+AmountOfLines = input("Enter Amount of Coordinates: ")
+
+HEIGHTSPACE = int(HEIGHT) / int(AmountOfLines)
+WIDTHSPACE = int(WIDTH) / int(AmountOfLines)
+
+heightSpace = int(HEIGHT) / int(AmountOfLines)
+widthSpace = int(WIDTH) / int(AmountOfLines)
+fontSizeX = int(WIDTH) / 25
+fontSizeY = int(HEIGHT) / 25
+
+
+
+with open('test.svg', 'w') as f:
+    f.write(f'<svg style="border:  green solid;" viewBox="0 0 {WIDTH} {HEIGHT}" xmlns="http://www.w3.org/2000/svg">')
+    for i in range(int(AmountOfLines)):
+        f.write(f'<path stroke="green" stroke-width=".3" d="M{heightSpace} 0 V{HEIGHT} "/>')
+        f.write(f'<path stroke="green" stroke-width=".3" d="M0 {widthSpace} H{WIDTH} "/>')
+
+        f.write(f'<text font-size="{fontSizeX}" x="{widthSpace}" y="1">{round(widthSpace)}</text>')
+        f.write(f'<text font-size="{fontSizeY}" y="{heightSpace}" x="1">{round(heightSpace)}</text>')
+
+        heightSpace += HEIGHTSPACE
+        widthSpace += WIDTHSPACE
+
+    f.write("<svg/>")
+    f.close()
